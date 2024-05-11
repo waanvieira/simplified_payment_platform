05caf58d80a393e75147d19b9d267f8d8eccaed7
wandersonvieira <wandersonvieira@movida.com.br>
2024-05-11 15:33:18 -0300

    fea: add api resources and routes

--- ms_account/routes/web.php
+++ ms_account/routes/web.php
@@ -1,16 +1,8 @@
 <?php
 
-use App\Http\Controllers\Api\AccountController;
-use App\Http\Controllers\Api\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::prefix('v1')->group(function () {
-    Route::apiResource('users', UserController::class);
-    Route::apiResource('accounts', AccountController::class);
-});
-
 
