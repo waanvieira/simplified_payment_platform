59a6643c87b8779ee9795dc781f07254db3cb624
wandersonvieira <wandersonvieira@movida.com.br>
2024-05-11 17:58:37 -0300

    fea: add dataBaseSeeder with two users default

--- ms_account/database/seeders/DatabaseSeeder.php
+++ ms_account/database/seeders/DatabaseSeeder.php
@@ -2,9 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Account;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Ramsey\Uuid\Uuid;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,9 +17,33 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
+        Account::factory()->create([
+            'id' => '7e0b17a3-2f57-4273-a48a-9f81ed2958eb',
+            'name' => 'user Payer test',
+            'cpf_cnpj' => '363.019.710-86',
             'email' => 'test@example.com',
+            'password' => '123456',
+            'balance' => 100
+        ]);
+
+        Account::factory()->create([
+            'id' => '8afd4fc5-5989-4b69-a799-1a0e2866235d',
+            'name' => 'Test Payee User',
+            'cpf_cnpj' => '657.287.100-26',
+            'email' => 'payee@example.com',
+            'password' => '123456',
+            'balance' => 50
+        ]);
+
+        Account::factory()->create([
+            'id' => '8d2b2858-d84a-4174-9a49-38fa95ed4bd6',
+            'name' => 'Test ShopKeeper User',
+            'cpf_cnpj' => '657.287.100-26',
+            'email' => 'payee@example.com',
+            'password' => '123456',
+            'shopkeeper' => true,
+            'balance' => 50
         ]);
     }
+
 }
