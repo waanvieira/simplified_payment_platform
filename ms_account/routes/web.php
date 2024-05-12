1cb903515194db88f70c8de8ad52651a60a5aaaa
wandersonvieira <wandersonvieira@movida.com.br>
2024-05-12 18:44:27 -0300

    fea: add health check route

--- ms_account/routes/web.php
+++ ms_account/routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\Http\Controllers\HealthCheckController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/heath', [HealthCheckController::class, 'health']);
--- ms_transactions/app/Http/Controllers/HealthCheckController.php
+++ ms_transactions/app/Http/Controllers/HealthCheckController.php
@@ -6,24 +6,6 @@ use Illuminate\Http\Response;
 
 class HealthCheckController extends Controller
 {
-    protected $sap;
-
-    // public function __construct()
-    // {
-    //     $this->sap = new SapClientInterface();
-    // }
-
-    /**
-     * @OA\Get(
-     *     tags={"Health"},
-     *     path="/health",
-     *      summary="Verifica o status da aplicação",
-     *      @OA\Response(response=202, description="Sistema no ar"),
-     *      @OA\Response(response=400, description="Bad Request"),
-     *      @OA\Response(response=404, description="End point não encontrado"),
-     *      @OA\Response(response=500, description="Erro interno")
-     * )
-     */
     public function health()
     {
         return response()->json([
