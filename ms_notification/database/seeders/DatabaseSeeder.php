127b7854609fe33a55d2471b0a7b55d8757ae9cb
wandersonvieira <wandersonvieira@movida.com.br>
2024-05-12 21:53:49 -0300

    fea: add news updates in containers

--- ms_notification/database/seeders/DatabaseSeeder.php
+++ ms_notification/database/seeders/DatabaseSeeder.php
@@ -15,22 +15,5 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
-
-        User::factory()->create([
-            'id' => Uuid::uuid4()->toString(),
-            'name' => 'user Payer test',
-            'cpf_cnpj' => '363.019.710-86',
-            'email' => 'test@example.com',
-            'password' => '123456',
-            'balance' => 100
-        ]);
-
-        User::factory()->create([
-            'id' => Uuid::uuid4()->toString(),
-            'name' => 'Test Payee User',
-            'cpf_cnpj' => '657.287.100-26',
-            'email' => 'payee@example.com',
-            'balance' => 50
-        ]);
     }
 }
